4e180b56897d3759de71ce2a78eed838df57e77d
dabman22 <47197645+dabman22@users.noreply.github.com>
2019-05-06 22:43:00 -0400

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+	
+	{
+		name: "David Brass",
+		photo: "http://pngimg.com/uploads/monkey/monkey_PNG18734.png",
+		scores: [
+			"5",
+			"5",
+			"2",
+			"1",
+			"5",
+			"3",
+			"4",
+			"2",
+			"3",
+			"1"
+		]
+	},
+	{
+		name: "joseph foster",
+		photo: "https://tse2.mm.bing.net/th?id=OIP.kcGFLc82vXxElkh_vpuD4wHaJ3&pid=Api&P=0&w=300&h=300",
+		scores: [
+			"3",
+			"2",
+			"4",
+			"4",
+			"2",
+			"3",
+			"5",
+			"2",
+			"5",
+			"4"
+		]
+	},
+	{
+		name: "Tanner Robinson",
+		photo: "https://tse4.mm.bing.net/th?id=OIP.PEc5rkrjHgG1sBuaZqXYpwHaE6&pid=Api&P=0&w=231&h=154",
+		score: [
+			"5",
+			"1",
+			"4",
+			"3",
+			"2",
+			"5",
+			"5",
+			"3",
+			"4",
+			"4"		
+		]
+	}
+];
+
+module.exports = friends;
